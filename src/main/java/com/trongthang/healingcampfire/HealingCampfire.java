1e8ef0b51b024948c44726dfefe52a9c43376db7
CaoTrongThang <fptisthebest@gmail.com>
2024-10-29 05:09:28 +0700

    just testing github

--- src/main/java/com/trongthang/healingcampfire/HealingCampfire.java
+++ src/main/java/com/trongthang/healingcampfire/HealingCampfire.java
@@ -9,7 +9,7 @@ public class HealingCampfire implements ModInitializer {
 	public static final String MOD_ID = "healing-campfire";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+	//my changes
 	@Override
 	public void onInitialize() {
 
