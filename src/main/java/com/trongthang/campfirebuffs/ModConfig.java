06b8d064bd4847d39234b0f5cd1bf009357ae4b1
CaoTrongThang <fptisthebest@gmail.com>
2024-11-03 04:34:46 +0700

    added an option "require_lit_campfire"

--- src/main/java/com/trongthang/campfirebuffs/ModConfig.java
+++ src/main/java/com/trongthang/campfirebuffs/ModConfig.java
@@ -1,7 +1,8 @@
-package com.trongthang.campfirebuff;
+package com.trongthang.campfirebuffs;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import net.fabricmc.loader.api.FabricLoader;
@@ -13,21 +14,27 @@ import java.io.IOException;
 import java.util.List;
 
 public class ModConfig {
-    private static final String CONFIG_FILE_NAME = "campfire_buff.json";
+    private static final String CONFIG_FILE_NAME = "campfire_buffs.json";
     private static ModConfig INSTANCE;
 
     @Expose
-    @SerializedName("healing_radius")
-    public int buffRadius = 5;
+    @SerializedName("buff_radius")
+    public int buffRadius = 6;
 
     @Expose
     @SerializedName("check_interval")
-    public int checkInterval = 20; // Check every 20 ticks
+    public int checkInterval = 30;
+
+    @Expose
+    @SerializedName("require_lit_campfire")
+    public boolean requireLitCampfire = true;
 
     @Expose
     @SerializedName("buffs")
-    public List<BuffConfig> buffs = List.of(new BuffConfig("minecraft:regeneration", 200, 0)
-    , new BuffConfig("minecraft:resistance", 200, 0)); // Default regeneration
+    public List<BuffConfig> buffs = List.of(
+            new BuffConfig("minecraft:regeneration", 200, 0),
+            new BuffConfig("minecraft:resistance", 200, 0)
+    ); // Default regeneration and resistance effects
 
     public static class BuffConfig {
         @Expose
@@ -56,18 +63,34 @@ public class ModConfig {
         // Load configuration from file if it exists
         if (configFile.exists()) {
             try (FileReader reader = new FileReader(configFile)) {
-                INSTANCE = gson.fromJson(reader, ModConfig.class);
+                JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+
+                // Check if "require_lit_campfire" is missing in the JSON file
+                boolean requireLitCampfireMissing = !jsonObject.has("require_lit_campfire");
+
+                // Load the full config as an instance of ModConfig
+                INSTANCE = gson.fromJson(jsonObject, ModConfig.class);
+
+                // If requireLitCampfire was missing, set the default value and save the config
+                if (requireLitCampfireMissing) {
+                    INSTANCE.requireLitCampfire = true;  // Set default value for requireLitCampfire
+                    saveConfig(gson, configFile);  // Save updated config with new field
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         } else {
             // Create a default config file if it doesn't exist
             INSTANCE = new ModConfig();
-            try (FileWriter writer = new FileWriter(configFile)) {
-                writer.write(gson.toJson(INSTANCE));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            saveConfig(gson, configFile);
+        }
+    }
+
+    private static void saveConfig(Gson gson, File configFile) {
+        try (FileWriter writer = new FileWriter(configFile)) {
+            writer.write(gson.toJson(INSTANCE));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
